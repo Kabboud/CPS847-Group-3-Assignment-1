e725687f2586553f770160fd2d6a15974d2d59cd
Kabboud <98329660+Kabboud@users.noreply.github.com>
2022-02-03 00:40:14 -0500

    Echo Bot Source Code

--- echobot.py
+++ echobot.py
@@ -1,4 +1,3 @@
-import logging
 from slack_bolt import App
 
 app = App()
@@ -17,7 +16,4 @@ def handle_app_mention_events(body, say):
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
     app.start(3000)
